c0811ef246a11e68207701d021cac30e6d3d1428
Justin Prince <justin.prince@chainguard.dev>
2026-04-20 11:26:28 -0400

    Removed old folders and such.

--- image-copy-acr/iac/container-app/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "container_app_url" {
-  value       = azurerm_container_app.image_copy.ingress[0].fqdn
-  description = "Public URL for the container app."
-}
--- image-copy-acr/iac/function/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "function_app_name" {
-  value       = azurerm_linux_function_app.image_copy.name
-  description = "Function App name."
-}
